ccf523663b307d3210c3a642e9d62f99c296c058
Tayler Haviland <havil016@umn.edu>
2018-11-12 20:04:25 -0600

    Outputs updated

---
+++ terraform/inet4007/output.tf
@@ -0,0 +1,37 @@
+output "public_prod_0_ips" {
+  description = "Public Prod 0 Public IPs"
+  value = ["${aws_instance.public_prod.0.public_ip}"
+  , "${aws_instance.public_prod.0.private_ip}"]
+}
+output "public_prod_1_ips" {
+  value = ["${aws_instance.public_prod.1.public_ip}"
+    ,"${aws_instance.public_prod.1.private_ip}"]
+}
+output "public_failover_0_ips" {
+  value = ["${aws_instance.public_failover.0.public_ip}"
+  ,"${aws_instance.public_failover.0.private_ip}"]
+}
+
+output "public_failover_1_ips" {
+  value = ["${aws_instance.public_failover.1.public_ip}"
+  ,"${aws_instance.public_failover.1.private_ip}"]
+}
+
+output "private_prod_0_ips" {
+  value = ["${aws_instance.private_prod.0.public_ip}"
+  ,"${aws_instance.private_prod.0.private_ip}"]
+}
+output "private_prod_1_ips" {
+  value = ["${aws_instance.private_prod.1.public_ip}"
+  ,"${aws_instance.private_prod.1.private_ip}"]
+}
+
+output "private_test_0_ips" {
+  value = ["${aws_instance.private_test.0.public_ip}"
+  ,"${aws_instance.private_test.0.private_ip}"]
+}
+
+output "private_test_1_ips" {
+  value = ["${aws_instance.private_test.1.public_ip}"
+  ,"${aws_instance.private_test.1.private_ip}"]
+}
